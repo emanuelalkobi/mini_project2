d53c7503529450c760fdc2d0c9738db2147ce71d
emanuel alkobi <emanuelalkobi@MacBook-Pro.local>
2018-10-10 09:32:37 -0400

    Implementation of TensorFlow Classifier Update main.py to support both classifiers Update README  file

--- README.md
+++ README.md
@@ -7,12 +7,19 @@ the output of the classifier should be a result that will represent the chance t
 my data set consist of 144 images with soccer ball and with 160 images with no soccer ball.
 
 
+Running my code API
+------------------------------------------------------------------------------------
+python main.py image_path
+
+you need to download all my project github and then run the main.py file with a path to the image you want to know if there is a soccer ball.
+
 I will implement 2 image classifires:
 1.I will train  The 1st classifier using the Google cloud AutoML Vision interface.
+2.I will train the 2nd classifier using TensorFlow.
 
-
-
-
+Google cloud AutoML info
+-------------------------------------------------------------------------------------
+in order to use this classifier you need to have the right credentials for this google cloud project.
 1.in order to run google cloud run the next commands:
 
 export GOOGLE_APPLICATION_CREDENTIALS="/Users/emanuelalkobi/Desktop/ec601/mini_project2/mini_project2/1.json"
@@ -31,3 +38,8 @@ export PROJECT_ID="clas-219000"
 export REGION_NAME="us-central1"
 
 gsutil mb -p $PROJECT_ID -c regional -l $REGION_NAME gs://$PROJECT_ID-vcm/
+
+TensorFlow
+-------------------------------------------------------------------------------------
+I retrain my data  set images over the Inception V3 architecture trained on ImageNet.
+In this method I chage the Inception last layer classifier only.
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import os
 import os.path
 import google_vision_api as google_vision_api
-
+import label_image as label_image
 if (len(sys.argv)!=2):
     print("Please insert a file name only")
     sys.exit(1)
@@ -15,10 +15,28 @@ if not os.path.isfile(file_name)  and not os.access(file_name, os.R_OK):
 print("Soccer ball image detector")
 print("Please choose with witch which classifier you want to predict your image")
 print("1.Google cloud AutoML Vision")
-print("2.")
+print("2.TensorFlow classifier")
+print("3.Run both")
 nb = input('Choose a classifier: ')
-if (int(nb)==1):
+if (int(nb)==1 or int(nb)==3):
+    print("---------------------------------------------------------------------------------")
     print("google classifier is running")
+    print("---------------------------------------------------------------------------------")
     with open(file_name, 'rb') as ff:
         content = ff.read()
-    print(google_vision_api.get_prediction(content, 'clas-219000',  'ICN7228006044013306359'))
+    response=(google_vision_api.get_prediction(content, 'clas-219000',  'ICN7228006044013306359'))
+    print("---------------------------------------------------------------------------------")
+    print("Google cloud AutoML vision results")
+    print("---------------------------------------------------------------------------------")
+    for result in response.payload:
+        print("Predicted class name: {}".format(result.display_name))
+        print("Predicted class score: {}".format(result.classification.score))
+if (int(nb)==2 or int(nb)==3):
+    print("---------------------------------------------------------------------------------")
+    print("TensorFlow classifier is running")
+    print("---------------------------------------------------------------------------------")
+    os.system('python label_image.py --graph=output_graph.pb --labels=output_labels.txt --input_layer=Placeholder  --output_layer=final_result --image='+file_name )
+else:
+    print("Please type a number between 1 to 3")
+    sys.exit(1)
+
